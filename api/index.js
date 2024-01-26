f6ccf3a316c470f22c081f1b3d3b56ba1039ca9e
ajayshirkeanzil <ajayshirke137@gmail.com>
2024-01-26 16:04:40 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from "express";
- const app = express()
- app.listen(3000,()=>{
-    console.log("server running on 3000")
- })
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then((e) => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err, "Error While Connecting to DB");
+  });
+
+app.listen(3000, () => {
+  console.log("server running on 3000");
+});

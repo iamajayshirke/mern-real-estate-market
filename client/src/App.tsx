fc6827ee2c6547e46c071c37b3080df097c983e8
ajayshirkeanzil <ajay.shrike@anzilsoft.com>
2024-01-26 14:48:21 +0530

    Header Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/profile" Component={Profile} />
+        <Route path="/about" Component={About} />
+        <Route path="/sign-in" Component={SignIn} />
+        <Route path="/sign-up" Component={SignUp} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCs0C4k_3wH4voSZts6nGRtOQEvkaU0P0Y",
+    authDomain: "mern-estate-36d48.firebaseapp.com",
+    projectId: "mern-estate-36d48",
+    storageBucket: "mern-estate-36d48.appspot.com",
+    messagingSenderId: "562950810552",
+    appId: "1:562950810552:web:afdd3dc0bc161e71d37d4d",
+    measurementId: "G-CLXBP49YGK"
+  };
